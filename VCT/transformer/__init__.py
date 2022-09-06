59a179f507a2cab6fa465db08b9541ce313ede21
tl32rodan <tl32rodan.cs09g@nctu.edu.tw>
2022-09-06 16:23:54 +0800

    Update

--- VCT/transformer/__init__.py
+++ VCT/transformer/__init__.py
@@ -6,10 +6,10 @@ import transformer.Modules
 import transformer.Layers
 import transformer.SubLayers
 import transformer.Models
-import transformer.Translator
+#import transformer.Translator
 import transformer.Optim
 
 __all__ = [
     transformer.Constants, transformer.Modules, transformer.Layers,
-    transformer.SubLayers, transformer.Models, transformer.Optim,
-    transformer.Translator]
+    transformer.SubLayers, transformer.Models, transformer.Optim,]
+    #transformer.Translator]
